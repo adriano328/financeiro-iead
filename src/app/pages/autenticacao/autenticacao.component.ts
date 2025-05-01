df30404a1cc5b1157dccf5e49d30156e5bed5626
adriano328 <lucasadrianodias@gmail.com>
2025-04-30 22:00:22 -0400

    feat: :hammer: Implementa tela de login
    Implementa tela de login com validação de usuario e senha + codigo de autenticator do google
    
    - Validação de senha, usuario e authenticator

--- src/app/pages/autenticacao/autenticacao.component.ts
+++ src/app/pages/autenticacao/autenticacao.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-autenticacao',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ButtonModule, TooltipModule],
   templateUrl: './autenticacao.component.html',
   styleUrl: './autenticacao.component.scss'
 })
